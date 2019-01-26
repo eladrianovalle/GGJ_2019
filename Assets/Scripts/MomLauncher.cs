5f0c3ef2d3b57a6b15c5d422975c6ce1f468bd44
Adriano Valle <adrianovalle@me.com>
2019-01-26 13:31:28 -0500

    mom launcher

---
+++ Assets/Scripts/MomLauncher.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MomLauncher : MonoBehaviour {
+
+	public GameObject[] throwableObjects;
+	public float throwInterval = 3.0f;
+
+	void Start () 
+	{
+		InvokeRepeating ("ThrowObject", throwInterval, throwInterval);
+	}
+	
+	void ThrowObject () 
+	{
+		Debug.Log ("Mom throws a thing!!!");
+	}
+}
