5837cb0d2ce9e35c0ea7e1e1893594c4b5ffaa2d
Adriano Valle <adrianovalle@me.com>
2019-01-26 14:48:33 -0500

    mom launcher

--- Assets/Scripts/MomLauncher.cs
+++ Assets/Scripts/MomLauncher.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class MomLauncher : MonoBehaviour {
 
+	public GameObject player;
+	public GameObject throwStart;
 	public GameObject[] throwableObjects;
 	public float throwInterval = 3.0f;
+	public float force = 5.0f;
 
 	void Start () 
 	{
@@ -15,7 +18,11 @@ public class MomLauncher : MonoBehaviour {
 	void ThrowObject () 
 	{
 		Debug.Log ("Mom throws a thing!!!");
-		GameObject objToThrow = throwableObjects [Random.Range(0, throwableObjects.Length)];
+		GameObject objToThrow = Instantiate(throwableObjects [Random.Range(0, throwableObjects.Length)]);
 
+		objToThrow.transform.position = throwStart.transform.position;
+		Rigidbody objToThrowRbody = objToThrow.GetComponent<Rigidbody> ();
+		Vector3 throwDirection = player.transform.position - objToThrow.transform.position;
+		objToThrowRbody.AddForce (throwDirection * force, ForceMode.Force);
 	}
 }
