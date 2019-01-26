795988c4f703204188af3568d4abb4358f7e7d40
Adriano Valle <adrianovalle@me.com>
2019-01-26 12:56:15 -0500

    game level and initial controller scripts

---
+++ Assets/Scripts/HandheldPlayer.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandheldPlayer : MonoBehaviour {
+
+	Rigidbody rBody;
+	public float moveSpeed = 2.0f;
+
+	void OnEnable()
+	{
+		GameController.OnHandheldMoveLeft 	+= MoveLeft;
+		GameController.OnHandheldMoveRight 	+= MoveRight;
+	}
+
+	void OnDisable()
+	{
+		GameController.OnHandheldMoveLeft 	-= MoveLeft;
+		GameController.OnHandheldMoveRight	-= MoveRight;
+	}
+
+	void Awake()
+	{
+		rBody = GetComponent<Rigidbody> ();
+	}
+
+	void Start () 
+	{
+		
+	}
+	
+//	void Update () 
+//	{
+//		if (Input.GetKeyDown (KeyCode.LeftArrow))
+//		{
+//			
+//		}
+//		else if (Input.GetKeyDown (KeyCode.RightArrow))
+//		{
+//			
+//		}
+//		else if (Input.GetKeyDown (KeyCode.UpArrow))
+//		{
+//			
+//		}
+//	}
+
+	void MoveLeft(bool isMovingLeft)
+	{
+		Debug.Log ("Move Left");
+
+		Vector3 movePosition = this.rBody.transform.position += (Vector3.left * moveSpeed) * Time.deltaTime;
+		rBody.MovePosition (movePosition);
+	}
+
+	void MoveRight(bool isMovingRight)
+	{
+		Debug.Log ("Move Right");
+		Vector3 movePosition = this.rBody.transform.position += (Vector3.right * moveSpeed) * Time.deltaTime;
+		rBody.MovePosition (movePosition);
+	}
+}
---
+++ Assets/Scripts/Managers/GameController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameController : MonoBehaviour {
+
+	public static Action<bool> OnHandheldMoveLeft;
+	public static Action<bool> OnHandheldMoveRight;
+	public static Action<bool> OnCharacterJump;
+
+
+
+
+	void Start () {
+		
+	}
+	
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.LeftArrow))
+		{
+			if (OnHandheldMoveLeft != null)
+			{
+				OnHandheldMoveLeft (true);
+			}
+		}
+		else if (Input.GetKey (KeyCode.RightArrow))
+		{
+			if (OnHandheldMoveRight != null)
+			{
+				OnHandheldMoveRight (true);
+			}
+		}
+		else if (Input.GetKeyDown (KeyCode.UpArrow))
+		{
+			if (OnCharacterJump != null)
+			{
+				OnCharacterJump (true);
+			}
+		}
+	}
+}
