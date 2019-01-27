150b84715975095439f3221febbcdc3b614d7d43
Adriano Valle <adrianovalle@me.com>
2019-01-27 11:58:17 -0500

    momma oooooooooohh

--- Assets/Scripts/MomLauncher.cs
+++ Assets/Scripts/MomLauncher.cs
@@ -7,16 +7,72 @@ public class MomLauncher : MonoBehaviour {
 	public GameObject player;
 	public GameObject throwStart;
 	public GameObject[] throwableObjects;
+
+	SpriteRenderer sRenderer;
+	public Sprite[] momsprites;
+
+	float momDelay = 3f;
 	public float throwInterval = 3.0f;
+	float throwTimer;
+	bool timerRunning = false;
+
+	public Vector3[] momPositions;
+
 	float force = 1.5f;
 	float heightMult = 3.0f;
 	public float throwTorqueForce = 5.0f;
 	public Vector2 minMaxThrowTorque;
+
+	void Awake()
+	{
+		sRenderer = GetComponent<SpriteRenderer> ();
+		throwTimer = throwInterval;
+	}
+
 	void Start () 
 	{
-		InvokeRepeating ("ThrowObject", throwInterval, throwInterval);
+//		InvokeRepeating ("ThrowObject", throwInterval, throwInterval);
+
+		MomLeaveRoom ();
+
+		LeanTween.delayedCall (momDelay, ()=>{
+			MomEnterRoom();
+			timerRunning = true;
+		});
 	}
-	
+
+	void Update()
+	{
+		if (timerRunning)
+		{
+			throwTimer -= Time.deltaTime;
+
+			if (throwTimer < 0)
+			{
+				MomEnterRoom ();
+
+				float newThrowInterval = throwInterval * 0.95f;
+				throwInterval = newThrowInterval;
+				throwTimer = throwInterval;
+			}
+
+		}
+	}
+
+	void MomEnterRoom()
+	{
+		LeanTween.moveLocal (this.gameObject, momPositions[1], 2f).setEase(LeanTweenType.easeOutQuint).setOnComplete(()=>{
+			ThrowObject();
+		});
+	}
+
+	void MomLeaveRoom()
+	{
+		LeanTween.moveLocal (this.gameObject, momPositions[0], 2f).setEase(LeanTweenType.easeInQuint).setOnComplete(()=>{
+			//close door and screenshake
+		});
+	}
+
 	void ThrowObject () 
 	{
 		if (GameController.gameOver)
@@ -24,7 +80,7 @@ public class MomLauncher : MonoBehaviour {
 			return;
 		}
 
-		Debug.Log ("Mom throws a thing!!!");
+//		Debug.Log ("Mom throws a thing!!!");
 		GameObject objToThrow = Instantiate(throwableObjects [Random.Range(0, throwableObjects.Length)]);
 
 		objToThrow.transform.position = throwStart.transform.position;
