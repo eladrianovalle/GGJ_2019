15d22376556d1a060b1df63c882c02182bc4ce57
Adriano Valle <adrianovalle@me.com>
2019-01-26 19:29:50 -0500

    retry working

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour {
 
 	public TextMeshProUGUI text;
 	public float startPosition_y = -1200;
 	bool hasTriggered = false;
+
+	public CanvasGroup retryPanel;
+	public Button retryButton;
+//	float originalPanelAlpha;
 
 	void OnEnable()
 	{
@@ -25,6 +31,7 @@ public class UIController : MonoBehaviour {
 	{
 		Vector3 startingPos = new Vector3 (this.transform.position.x, startPosition_y, this.transform.position.z);
 		text.transform.position = startingPos;
+		HideRetryPanel ();
 	}
 
 	void ShowWinText()
@@ -35,9 +42,11 @@ public class UIController : MonoBehaviour {
 		}
 		hasTriggered = true;
 
-		LeanTween.moveY (text.rectTransform, 0, 2f).setEase(LeanTweenType.easeOutBounce);
 		text.color = Color.green;
 		text.text = "YOU\nWIN";
+		LeanTween.moveY (text.rectTransform, 0, 2f).setEase(LeanTweenType.easeOutBounce).setOnComplete(()=>{
+			ShowRetryPanel();
+		});
 	}
 
 	void ShowLoseText()
@@ -48,9 +57,39 @@ public class UIController : MonoBehaviour {
 		}
 		hasTriggered = true;
 
-		LeanTween.moveY (text.rectTransform, 0, 2f).setEase(LeanTweenType.easeOutBounce);
 		text.color = Color.red;
 		text.text = "YOU\nLOSE";
+		LeanTween.moveY (text.rectTransform, 0, 2f).setEase(LeanTweenType.easeOutBounce).setOnComplete(()=>{
+			ShowRetryPanel();
+		});	
+	}
+
+	void HideRetryPanel()
+	{
+		retryPanel.alpha = 0;
+		retryButton.interactable = false;
+	}
+
+	void ShowRetryPanel()
+	{
+		LeanTween.alphaCanvas (retryPanel, 1f, 1f).setEase(LeanTweenType.easeOutExpo).setOnComplete(()=>{
+//			retryPanel.blocksRaycasts = false;
+			retryButton.interactable = true;
+
+			retryButton.onClick.RemoveAllListeners();
+			Debug.Log("Let's add the RestartGame func, son!!!!");
+			retryButton.onClick.AddListener(()=>{
+				RestartGame();
+			});
+		});
+	}
+
+	public void RestartGame()
+	{
+		Debug.Log ("Let's load the scene over!!!");
+		GameController.gameOver = false;
+		Scene thisScene = SceneManager.GetActiveScene ();
+		SceneManager.LoadScene (thisScene.buildIndex);
 	}
 
 }
