3cee474cf9c3a680689270dd6f593ee09163be36
OrigamiCoder <OrigamiCoder@gmail.com>
2019-01-27 03:43:50 -0500

    Ninja Guy Dan game loop functional

--- Assets/Scripts/Handheld/HandheldGame.cs
+++ Assets/Scripts/Handheld/HandheldGame.cs
@@ -8,6 +8,7 @@ public class HandheldGame : MonoBehaviour
 	[SerializeField]
 	[Tooltip("Frames per GameLoop update")]
 	protected int frames = 20;
+	private int currFrames = 0;
 
 	[SerializeField]
 	protected const int COLUMNS = 5;
@@ -16,10 +17,26 @@ public class HandheldGame : MonoBehaviour
 
 
 	// Events
+	/// <summary>
+	/// The character jump event.
+	/// </summary>
 	public static Action OnCharacterJump;
+	/// <summary>
+	/// The character fall event.
+	/// </summary>
 	public static Action OnCharacterFall;
+	/// <summary>
+	/// The game start event.
+	/// </summary>
 	public static Action OnGameStart;
+	/// <summary>
+	/// The game over event.
+	/// </summary>
 	public static Action OnGameOver;
+	/// <summary>
+	/// The game loop update event.
+	/// </summary>
+	public static Action OnGameLoopUpdate;
 
 	// GameObjects
 	[Header("Screen Objects")]
@@ -29,13 +46,24 @@ public class HandheldGame : MonoBehaviour
 
 	[SerializeField] private SpriteRenderer[] Buildings = new SpriteRenderer[COLUMNS];
 
+	[SerializeField] private SpriteRenderer Battery;
+
+	private const int SCORE_DIGITS = 3;
+	[SerializeField] private SpriteRenderer[] Score = new SpriteRenderer[SCORE_DIGITS];
+
+	[SerializeField] private GameObject GameOver;
+
+	[Header("Platform Sprites")]
 	[SerializeField] private Sprite buildingSprite;
 	[SerializeField] private Sprite pitSprite;
 
+	private const int BATT_COUNT = 4;
+	[SerializeField] private Sprite[] batterySprites = new Sprite[BATT_COUNT];
+
 
 	// Properties
-	protected HandheldCharacter character;
-	protected Queue<int> platformValues;
+	protected HandheldCharacter character = new HandheldCharacter();
+	protected List<int> platformValues;
 
 	protected bool buttonPressed = false;
 
@@ -61,45 +89,120 @@ public class HandheldGame : MonoBehaviour
 
 	public void Init()
 	{
-		Debug.Assert(NinjaJump != null, "NinjaJump object missing!", this.gameObject);
-		Debug.Assert(NinjaStand != null, "NinjaStand object missing!", this.gameObject);
-		Debug.Assert(NinjaFall != null, "NinjaFall object missing!", this.gameObject);
+		Debug.Assert(NinjaJump != null, "[HandheldGame] NinjaJump object missing!", this.gameObject);
+		Debug.Assert(NinjaStand != null, "[HandheldGame] NinjaStand object missing!", this.gameObject);
+		Debug.Assert(NinjaFall != null, "[HandheldGame] NinjaFall object missing!", this.gameObject);
 		for (int i = 0; i < COLUMNS; i++)
 		{
-			Debug.Assert(Buildings[i] != null, "Building" + i + " object missing!", this.gameObject);
+			Debug.Assert(Buildings[i] != null, "[HandheldGame] Building" + i + " object missing!", this.gameObject);
 		}
 
-		character = new HandheldCharacter();
-		platformValues = new Queue<int>(startingPlatformValues);
+		character.Init();
+		platformValues = new List<int>(startingPlatformValues);
 
 		CurrentGameState = HandheldGameState.START;
 
 	}
 
+	void Start()
+	{
+		Init();
+	}
+
 	void Update()
 	{
-		if (CurrentGameState == HandheldGameState.START)
+		switch (CurrentGameState)
 		{
-			if (buttonPressed)
-			{
-				buttonPressed = false;
-				CurrentGameState = HandheldGameState.PLAYING;
-				Debug.Log("Handheld game start");
-			}
+			case HandheldGameState.START:
+				StartUpdate();
+				break;
+			case HandheldGameState.PLAYING:
+				PlayingUpdate();
+				break;
+			case HandheldGameState.END:
+				EndUpdate();
+				break;
+			case HandheldGameState.CREDITS:
+				break;
 		}
 
 		DrawScreen();
 	}
 
+	private void StartUpdate()
+	{
+		if (buttonPressed)
+		{
+			buttonPressed = false;
+			//Init();
+			CurrentGameState = HandheldGameState.PLAYING;
+			Debug.Log("[HandheldGame] Game start");
+			if (OnGameStart != null)
+			{
+				OnGameStart();
+			}
+		}
+	}
+
+	private void EndUpdate()
+	{
+		if (buttonPressed)
+		{
+			buttonPressed = false;
+			GameOver.SetActive(false);
+			//CurrentGameState = HandheldGameState.START;
+			Init();
+			Debug.Log("[HandheldGame] Leaving end screen");
+		}
+		else
+		{
+			if (++currFrames >= frames)
+			{
+				currFrames = 0;
+				RunGameLoop();
+			}
+		}
+	}
+
+	private void PlayingUpdate()
+	{
+		if (++currFrames >= frames)
+		{
+			currFrames = 0;
+			RunGameLoop();
+		}
+	}
+
 	public void RunGameLoop()
 	{
 		// Update Game Loop
+		Debug.Log("[HandheldGame] RunGameLoop()");
+		if (OnGameLoopUpdate != null)
+		{
+			OnGameLoopUpdate();
+		}
 
 		// GameState?
 		if (CurrentGameState == HandheldGameState.PLAYING)
 		{
+
+			// UpdateGround
+			for (int i = 0; i < COLUMNS; i++)
+			{
+				if (i + 1 < COLUMNS)
+				{
+					platformValues[i] = platformValues[i + 1];
+				}
+				else
+				{
+					platformValues[i] = (platformValues[i - 1] == 0) ? 1 : UnityEngine.Random.Range(0, 2);
+				}
+			}
+
 			// Check input
 			// if input, then try actions (jump)
+			Debug.Log("[HandheldGame] Button was pressed: " + buttonPressed);
+
 			bool jump = false;
 			if (buttonPressed)
 			{
@@ -109,23 +212,45 @@ public class HandheldGame : MonoBehaviour
 			bool fall = false;
 			if (jump)
 			{
-				OnCharacterJump();
+				Debug.Log("[HandheldGame] Jumped");
+				if (OnCharacterJump != null)
+				{
+					OnCharacterJump();
+				}
 			}
 			else
 			{
-				fall = character.TryFall();
+				character.TryStand();
+				if (platformValues[0] == 0)
+				{
+					fall = character.TryFall();
+				}
 			}
 
 			if (fall)
 			{
-				OnCharacterFall();
-				// Lives/Game Over
+				Debug.Log("[HandheldGame] Fell");
+				if (OnCharacterFall != null)
+				{
+					OnCharacterFall();
+				}
+				// Lives/Game Over?
+				CurrentGameState = HandheldGameState.END;
+				if (OnGameOver != null)
+				{
+					OnGameOver();
+				}
 			}
 
 			buttonPressed = false;
 
 			// Draw state?
 			DrawScreen();
+
+		}
+		else if (CurrentGameState == HandheldGameState.END)
+		{
+			GameOver.SetActive(!GameOver.activeSelf);
 		}
 	}
 
@@ -135,12 +260,10 @@ public class HandheldGame : MonoBehaviour
 		NinjaStand.SetActive(character.CurrentState == HandheldCharacter.CharacterState.STANDING);
 		NinjaFall.SetActive(character.CurrentState == HandheldCharacter.CharacterState.FALLING);
 
-		int index = 0;
-		foreach (int val in platformValues)
+		for (int i = 0; i < COLUMNS; i++)
 		{
-			Buildings[index].sprite = (val != 0) ? buildingSprite : pitSprite;
+			Buildings[i].sprite = (platformValues[i] != 0) ? buildingSprite : pitSprite;
 		}
-
 	}
 
 	private void ButtonPress(bool b)
