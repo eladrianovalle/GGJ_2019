95c1d39424665c0705b9e6b6b00c9b7680c03daf
Adriano Valle <adrianovalle@me.com>
2019-01-27 12:36:39 -0500

    door animation

--- Assets/Scripts/DoorAnimation.cs
+++ Assets/Scripts/DoorAnimation.cs
@@ -7,7 +7,7 @@ public class DoorAnimation : MonoBehaviour {
 	public GameObject door;
 	Vector3 openRot 	= new Vector3 (0, 9.5f, 0);
 	Vector3 closedRot 	= new Vector3 (0, 120f, 0);
-
+	float doorMoveSpeed = 0.5f;
 
 	void OnEnable () 
 	{
@@ -23,15 +23,15 @@ public class DoorAnimation : MonoBehaviour {
 	
 	void OpenDoor () 
 	{
-//		LeanTween.rotate (door, openRot, 1f).setEase(LeanTweenType.easeShake);
+		LeanTween.rotate (door, openRot, doorMoveSpeed).setEase(LeanTweenType.easeShake);
 //		LeanTween.rotateLocal (door, openRot, 1f).setEase(LeanTweenType.easeShake);
-		door.transform.localRotation = Quaternion.Slerp (door.transform.localRotation, Quaternion.Euler(openRot), Time.deltaTime * 5f);
+//		door.transform.localRotation = Quaternion.Slerp (door.transform.localRotation, Quaternion.Euler(openRot), Time.deltaTime * 5f);
 	}
 
 	void CloseDoor ()
 	{
-//		LeanTween.rotate (door, closedRot, 1f).setEase(LeanTweenType.easeShake);
+		LeanTween.rotate (door, closedRot, doorMoveSpeed).setEase(LeanTweenType.easeShake);
 //		LeanTween.rotateLocal (door, closedRot, 1f).setEase(LeanTweenType.easeShake);
-		door.transform.localRotation = Quaternion.Slerp (door.transform.localRotation, Quaternion.Euler(closedRot), Time.deltaTime * 5f);
+//		door.transform.localRotation = Quaternion.Slerp (door.transform.localRotation, Quaternion.Euler(closedRot), Time.deltaTime * 5f);
 	}
 }
