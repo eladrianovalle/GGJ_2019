bb95c4d2647b1b7bf7ed887949c796f6ab7f0e44
Lucas Edwards <lucas@redletteraudio.com>
2019-01-27 14:20:03 -0500

    Sounds Added. Controller Script

---
+++ Assets/AudioController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour {
+
+	public AK.Wwise.Event GameMusic;
+	public AK.Wwise.Event YouWin;
+	public AK.Wwise.Event YouLose;
+	public AK.Wwise.Event ImpactGood;
+	public AK.Wwise.Event ImpactBad;
+	public AK.Wwise.Event MomDialogue;
+	public AK.Wwise.Event MomOutside;
+	public AK.Wwise.Event PlayerJump;
+
+
+	void OnEnable()
+	{
+		ThrownObject.OnObjectHit += PlayHitSound;
+		MomLauncher.OnMomThrow += PlayMomDialogue;
+		GameController.OnPlayerWinGame += PlayYouWin;
+		HandheldGame.OnCharacterJump += PlayJumpSound;
+		HandheldGame.OnGameStart += PlayGameMusic;
+	}
+
+	void OnDisable(){
+		ThrownObject.OnObjectHit -= PlayHitSound;
+		MomLauncher.OnMomThrow -= PlayMomDialogue;
+		GameController.OnPlayerWinGame -= PlayYouWin;
+		HandheldGame.OnCharacterJump -= PlayJumpSound;
+		HandheldGame.OnGameStart -= PlayGameMusic;
+
+	}
+
+
+
+
+	// Music Sounds
+	void PlayGameMusic()
+	{
+		GameMusic.Post (gameObject);
+	}
+
+	void PlayYouWin()
+	{
+		YouWin.Post (gameObject);
+	}
+
+	void PlayYouLose ()
+	{
+		YouLose.Post (gameObject);
+	}
+
+
+	// Impact Sounds
+
+	void PlayHitSound(bool isGood)
+	{
+		if (isGood) 
+		{
+			ImpactGood.Post (gameObject);
+		}
+		else 
+		{
+			ImpactBad.Post (gameObject);
+		}
+	}
+
+
+	// Mom Sounds
+	void PlayMomDialogue()
+	{
+		MomDialogue.Post (gameObject);
+	}
+
+	void PlayMomOutside()
+	{
+		MomOutside.Post (gameObject);
+	}
+
+	// SFX
+
+	void PlayJumpSound ()
+	{
+		PlayerJump.Post (gameObject);
+	}
+
+
+
+
+
+
+}
