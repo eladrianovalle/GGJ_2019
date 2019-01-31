656f392b0f5137e4b816042885c934605aee3fce
Roman Sharf <pencilking2002@gmail.com>
2019-01-31 17:13:07 -0500

    Removes Ryder files from gitignore. Adds high score logic. Adds a high score white board prefab with a stand in model.

---
+++ Assets/Editor/HighScoreControllerInspector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(HighScoreController))]
+public class HighScoreControllerInspector : Editor {
+
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+
+		HighScoreController scoreController = (HighScoreController) target;
+		
+		if (GUILayout.Button("Add 10 to high score"))
+		{
+			scoreController.AddToHighScore(10);
+		}
+		
+		if (GUILayout.Button("Remove 10 from high score"))
+		{
+			scoreController.RemoveFromScore(10);
+		}
+
+		if (GUILayout.Button("Reset High Score"))
+		{
+			scoreController.ResetHighScore();
+		}
+	}
+}
