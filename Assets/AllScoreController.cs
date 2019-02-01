1f4161e2ff9be18c20a1c6b462ccf7188a9a8dd6
Roman Sharf <pencilking2002@gmail.com>
2019-02-01 18:40:47 -0500

    more work on random usernames

--- Assets/AllScoreController.cs
+++ Assets/AllScoreController.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 
@@ -7,5 +7,23 @@ public class AllScoreController : MonoBehaviour
 {
 
     public TextMeshProUGUI[] highScoreEntries;
-   
+    public RandomUserNameContainer randomUsernames;
+
+    private List<string> usernameList;
+
+    private void Awake()
+    {
+        usernameList = randomUsernames.userNames.ToList();
+    }
+
+    public string PopRandomUsername()
+    {
+        int index = Random.Range(0, usernameList.Count);
+        string username = usernameList[index];
+        usernameList.RemoveAt(index);
+
+        return username;
+
+    }
+
 }
