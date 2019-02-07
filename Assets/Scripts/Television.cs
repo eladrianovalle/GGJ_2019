9fad766f2e2b65cdbad2ab704e4bdc24a97f8981
OrigamiCoder <OrigamiCoder@gmail.com>
2019-02-06 20:50:16 -0500

    Add television animation idle state

---
+++ Assets/Scripts/Television.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+public class Television : MonoBehaviour
+{
+	public static Action<bool> PlayStatic;
+
+	[SerializeField]
+	private Animator animator;
+
+	void OnEnable()
+	{
+		Television.PlayStatic += SetStatic;	
+	}
+
+	void OnDisable()
+	{
+		Television.PlayStatic -= SetStatic;
+	}
+
+	void Start()
+	{
+		if (animator == null)
+		{
+			gameObject.SetActive(false);
+		}
+	}
+
+	public void SetStatic(bool enabled)
+	{
+		animator.Play(enabled ? "StaticAnim" : "Idle");
+	}
+}
+
