bb478c0ed5fe5c033e0ecb1ea98f921e02372f48
Adriano Valle <adrianovalle@me.com>
2019-02-07 22:00:38 -0500

    high score entry script for handling tmpro name and score

---
+++ Assets/Scripts/HighScoreEntry.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HighScoreEntry : MonoBehaviour {
+
+	public TextMeshProUGUI nameText;
+	public TextMeshProUGUI scoreText;
+
+	public string entryName 	{ get; private set; }
+	public string entryScore 	{ get; private set; }
+	
+	void SetScore (string name, string score) 
+	{
+		this.entryName 	= name;
+		this.entryScore = score;
+
+		nameText.text 	= name;
+		scoreText.text 	= score;
+	}
+}
