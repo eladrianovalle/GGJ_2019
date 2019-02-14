1b2e4d5ecf8eca87b070b1ccbd2672ace879c866
OrigamiCoder <OrigamiCoder@gmail.com>
2019-02-14 14:49:31 -0500

    Alternative camera setup

---
+++ Assets/Scripts/CameraBreathing.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class CameraBreathing : MonoBehaviour {
+
+	Vector3 lastLocalPosition;
+	Vector3 targetLocalPosition;
+
+	public float breathingRadius = 0.3f;
+	public float breathingDamping = 0.2f;
+
+	private float currTime = 0f;
+
+	void Start ()
+	{
+		UpdateTargetPosition();
+	}
+
+	void LateUpdate ()
+	{
+		
+		//targetPosition = new Vector3(targetTransform.position.x, transform.position.y, transform.position.z);
+
+		currTime += Time.deltaTime * breathingDamping;
+
+		transform.localPosition = Vector3.Lerp (lastLocalPosition, targetLocalPosition, currTime);
+		if (transform.localPosition == targetLocalPosition)
+		{
+			currTime = 0f;
+			UpdateTargetPosition();
+		}
+	}
+
+	void UpdateTargetPosition()
+	{
+		lastLocalPosition = transform.localPosition;
+		Vector2 randomPosition = Random.insideUnitCircle * breathingRadius;
+		targetLocalPosition = new Vector3(randomPosition.x, randomPosition.y);
+	}
+}
---
+++ Assets/Scripts/CameraDolly.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class CameraDolly : MonoBehaviour {
+
+	public Transform targetTransform;
+	Vector3 targetPosition;
+	Quaternion targetRotation;
+
+	float distance; 	// distance in the z plane
+	float height;		// height in the y plane
+
+	public float heightDamping = 2.0f;
+	public float distanceDamping = 10.0f;//0.6f;
+	public float rotationDamping = 10.0f;
+
+	public float breathingRadius = 0.5f;
+
+	public bool keepOriginalRotation = true;
+	public bool smoothRotation = true;
+	public bool followBehind = true;
+
+	void Start ()
+	{
+		if (!HaveTarget())
+		{
+			return;
+		}
+
+		// set starting values
+		distance = targetTransform.position.z - transform.position.z;
+		height = transform.position.y;
+	}
+
+	void LateUpdate ()
+	{
+		if (!HaveTarget())
+		{
+			return;
+		}
+
+		//		if (followBehind)
+		//		{
+		//			targetPosition = targetTransform.TransformPoint (targetTransform.position.x, height, -distance);
+		//		}
+		//		else
+		//		{
+		//			targetPosition = targetTransform.TransformPoint (targetTransform.position.x, height, distance);
+		//		}
+
+		targetPosition = new Vector3(targetTransform.position.x, transform.position.y, transform.position.z);
+
+		transform.position = Vector3.Lerp (transform.position, targetPosition, Time.deltaTime * distanceDamping);
+	}
+
+	bool HaveTarget()
+	{
+		bool haveTarget = true;
+		if (targetTransform == null)
+		{
+			haveTarget = false;
+			Debug.Log ("NO TARGET! I'M OUT!!!");
+		}	
+		return haveTarget;
+	}
+}
