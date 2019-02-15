da717e6cdaa888cdfb2ea385cbf408b505bbef50
OrigamiCoder <OrigamiCoder@gmail.com>
2019-02-15 02:33:44 -0500

    Camera breathing

--- Assets/Scripts/CameraBreathing.cs
+++ Assets/Scripts/CameraBreathing.cs
@@ -6,8 +6,8 @@ public class CameraBreathing : MonoBehaviour {
 	Vector3 targetLocalPosition;
     private float lowestY;
 
-	public float breathingRadius = 0.3f;
-	public float breathingDamping = 0.2f;
+	public float breathingRadius = 0.075f;
+	public float breathingDamping = 1f;
 
 	private float currTime = 0f;
 
@@ -22,14 +22,18 @@ public class CameraBreathing : MonoBehaviour {
 		
 		//targetPosition = new Vector3(targetTransform.position.x, transform.position.y, transform.position.z);
 
-		currTime += (1 - Mathf.Exp(-20 * Time.unscaledDeltaTime)) * breathingDamping;
+//		currTime += (1 - Mathf.Exp(-20 * Time.unscaledDeltaTime)) * breathingDamping;
+//
+//		transform.localPosition = Vector3.Lerp (lastLocalPosition, targetLocalPosition, currTime);
+//		if (transform.localPosition == targetLocalPosition)
+//		{
+//			currTime = 0f;
+//			UpdateTargetPosition();
+//		}
 
-		transform.localPosition = Vector3.Lerp (lastLocalPosition, targetLocalPosition, currTime);
-		if (transform.localPosition == targetLocalPosition)
-		{
-			currTime = 0f;
-			UpdateTargetPosition();
-		}
+		float breath = Mathf.Sin(Time.time * breathingDamping) * breathingRadius;
+		transform.localPosition = new Vector3(transform.localPosition.x, breath, transform.localPosition.z);
+		//this.transform.position = new Vector3(this.transform.position.x, posY + speed, this.transform.position.z);
 	}
 
 	void UpdateTargetPosition()
